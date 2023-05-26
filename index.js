a2cb1f737badbf637ad329d1d9806d3b86c81b8f
mrbll48 <william.marks87@gmail.com>
2023-05-25 17:54:51 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Square, Circle, Svg } = require("./lib/shapes.js");
+
+const questions = [
+  {
+    type: "input",
+    message: "What text do you want (3 characters max)",
+    name: "text",
+  },
+  {
+    type: "input",
+    message: "What text color would you like?",
+    name: "color",
+  },
+  {
+    type: "list",
+    message: "What shape would you like to use?",
+    name: "shape",
+    choices: ["circle", "triangle", "square"],
+  },
+  {
+    type: "input",
+    message: "What background color would you like?",
+    name: "background",
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  let shape;
+  switch (answers.shape) {
+    case "circle":
+      shape = new Circle(answers.background);
+      break;
+
+    case "triangle":
+      shape = new Triangle(answers.background);
+      break;
+
+    case "square":
+      shape = new Square(answers.background);
+      break;
+  }
+
+  const svg = new Svg(answers.text, answers.color, shape.render());
+  const completeSvg = svg.render();
+
+  fs.writeFile("logo.svg", completeSvg, (err) =>
+    err ? console.log(err) : console.log("successful")
+  );
+});
